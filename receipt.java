dad67b017f332ed9b2de0d6470f538e457959b27
Liver S. <szabooli24@7hills.org>
2023-08-30 13:04:08 -0400

    hello twyford

--- receipt.java
+++ receipt.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class receipt {
     public static void main(String args[]) {
-        System.out.println("Hello world!");
+        System.out.println("Hey whats up!");
+        System.out.println("Nothing nothing nothign nothign");
     }
 }
