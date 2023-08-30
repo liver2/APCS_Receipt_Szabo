b3a8d4125bba51d53f5ab7b641e0376684f1b81f
Liver S. <szabooli24@7hills.org>
2023-08-30 13:09:11 -0400

    change number 3

--- receipt.java
+++ receipt.java
@@ -4,5 +4,6 @@ public class receipt {
     public static void main(String args[]) {
         System.out.println("Hey whats up!");
         System.out.println("Nothing nothing nothign nothign");
+        System.out.println("Making a change");
     }
 }
