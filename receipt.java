1fa55048d2516eb6be23b0b198c6fd2a97cfe4ff
Liver S. <szabooli24@7hills.org>
2023-09-01 12:17:05 -0400

    receipt: adding menu and while loop for ordering

--- receipt.java
+++ receipt.java
@@ -2,10 +2,19 @@ import java.util.Scanner;
 
 public class receipt {
     public static void main(String args[]) {
-        System.out.println("Hey whats up!");
-        System.out.println("Nothing nothing nothign nothign");
-        System.out.println("Making a change");
-        System.out.println("hello world ig");
-        System.out.println("manifest !!");
+        boolean ordering = true;
+        Scanner menuScan = new Scanner(System.in);
+
+        System.out.println("Welcome to Liver's Rat Food Store!"); // welcome message
+        while (ordering == true) {
+            System.out.println("Please select an item from the following menu by typing the number or the name of the item.");
+            System.out.println("1. Rat Food Pellets   || $2.00");
+            System.out.println("2. Rat Ice Cream      || $4.50");
+            System.out.println("3. Rat-Fil-A Sandwich || $6.00");
+            System.out.println("4. Rat Fries          || $3.00");
+            System.out.println("5. Rat Tea            || $1.50");
+        }
+        
+        
     }
 }
