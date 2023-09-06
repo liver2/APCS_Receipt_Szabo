f8dac7668eeadb4ae38808b62859b5d4b2d222ad
Liver S. <szabooli24@7hills.org>
2023-09-06 12:15:41 -0400

    fixing price, array readings, etc

--- receipt.java
+++ receipt.java
@@ -4,15 +4,14 @@ public class receipt {
     public static void main(String args[]) {
         System.out.println("Welcome to Liver's Rat Food Store!"); // welcome message
 
-        int[][] order = new int[1][];
+        int[][] order = new int[2][3];
         boolean ordering = true;
         Scanner menuScan = new Scanner(System.in);
-        Scanner keepOrderingScan = new Scanner(System.in);
+        Scanner menuScan2 = new Scanner(System.in);
         int orderingCounter = 0;
-        String keepOrdering;
 
-        while (ordering == true) {
-            System.out.println("Please select an item from the following menu by typing the number of the item.");
+        while (ordering == true && orderingCounter < 3) {
+            System.out.println("Please select an item from the following menu by typing the number of the item (3 items).");
             System.out.println("1. Rat Food Pellets   || $2.20");
             System.out.println("2. Rat Ice Cream      || $4.40");
             System.out.println("3. Rat-Fil-A Sandwich || $6.60");
@@ -21,28 +20,26 @@ public class receipt {
             order[0][orderingCounter] = menuScan.nextInt();
 
             System.out.println("How many would you like?");
-            order[1][orderingCounter] = menuScan.nextInt();
-            
-            System.out.println("Would you like to keep ordering? (y/n)");
-            do {
-                keepOrdering = keepOrderingScan.nextLine();
-                if (keepOrdering == "y") {
-                    ordering = true;
-                } else if (keepOrdering == "n") {
-                    ordering = false;
-                }
-            } while (keepOrdering != "y" && keepOrdering != "n");    
+            order[1][orderingCounter] = menuScan2.nextInt();
 
             orderingCounter++;
         } // phase 1: ordering
 
         double price = 0;
 
-        for (int i = 0; i <= orderingCounter; i++) {
+        for (int i = 0; i < orderingCounter; i++) {
             price += order[1][i]*order[0][i]*(2.2);
         }
 
-        System.out.println("Thanks for shopping with Liver's Rat Food! Your total price is $" + price); //phase 2: price charge
+        System.out.println("Thanks for shopping with Liver's Rat Food! Your total price is $" + String.format("%.2f", price)); //phase 2: price charge
+
+        System.out.println("Here's your receipt!");
+
+        System.out.println("****************************************");
+        
+        for (int j = 0; j < orderingCounter; j++) {
+            
+        }
         
         
     }
