7325d22cd02d0b11942c2d2d1a55364482b09486
Liver S. <szabooli24@7hills.org>
2023-09-06 12:29:18 -0400

    almost done :>

--- receipt.java
+++ receipt.java
@@ -11,16 +11,20 @@ public class receipt {
         int orderingCounter = 0;
 
         while (ordering == true && orderingCounter < 3) {
-            System.out.println("Please select an item from the following menu by typing the number of the item (3 items).");
-            System.out.println("1. Rat Food Pellets   || $2.20");
-            System.out.println("2. Rat Ice Cream      || $4.40");
-            System.out.println("3. Rat-Fil-A Sandwich || $6.60");
-            System.out.println("4. Rat Fries          || $8.80");
-            System.out.println("5. Rat Tea            || $11.00");
-            order[0][orderingCounter] = menuScan.nextInt();
-
-            System.out.println("How many would you like?");
-            order[1][orderingCounter] = menuScan2.nextInt();
+            do {
+                System.out.println("Please select an item from the following menu by typing the number of the item (3 items).");
+                System.out.println("1. Rat Food Pellets   || $2.20");
+                System.out.println("2. Rat Ice Cream      || $4.40");
+                System.out.println("3. Rat-Fil-A Sandwich || $6.60");
+                System.out.println("4. Rat Fries          || $8.80");
+                System.out.println("5. Rat Tea            || $11.00");
+                order[0][orderingCounter] = menuScan.nextInt();
+            } while (order[0][orderingCounter] < 0 && order[0][orderingCounter] > 6);
+ 
+            do {
+                System.out.println("How many would you like? (Max 9)");
+                order[1][orderingCounter] = menuScan2.nextInt();
+            } while (order[1][orderingCounter] < 0 && order[1][orderingCounter] > 10);
 
             orderingCounter++;
         } // phase 1: ordering
@@ -31,16 +35,31 @@ public class receipt {
             price += order[1][i]*order[0][i]*(2.2);
         }
 
-        System.out.println("Thanks for shopping with Liver's Rat Food! Your total price is $" + String.format("%.2f", price)); //phase 2: price charge
-
+        System.out.println("Thanks for shopping with Liver's Rat Food! Your total price is $" + String.format("%.2f", price) + "."); //phase 2: price charge
+        System.out.println("");
         System.out.println("Here's your receipt!");
+        System.out.println("");
 
         System.out.println("****************************************");
         
         for (int j = 0; j < orderingCounter; j++) {
-            
+            if (order[0][j] == 1) {
+                System.out.println("Rat Food Pellets x " + order[1][j] + " || $" + order[1][j]*order[0][j]*(2.2) + "0");
+            } else if (order[0][j] == 2) {
+                System.out.println("Rat Ice Cream x " + order[1][j] + " || $" + order[1][j]*order[0][j]*(2.2) + "0");
+            } else if (order[0][j] == 3) {
+                System.out.println("Rat-Fil-A Sandwich x " + order[1][j] + " || $" + order[1][j]*order[0][j]*(2.2) + "0");
+            } else if (order[0][j] == 4) {
+                System.out.println("Rat Fries x " + order[1][j] + " || $" + order[1][j]*order[0][j]*(2.2) + "0");
+            } else if (order[0][j] == 5) {
+                System.out.println("Rat Tea x " + order[1][j] + " || $" + order[1][j]*order[0][j]*(2.2) + "0");
+            }
         }
-        
+        System.out.println("");
+        System.out.println("________________________________________");
+        System.out.println("");
+        System.out.println("Total Price: $" + String.format("%.2f", price) + "");
+        System.out.println("****************************************");
         
     }
 }
